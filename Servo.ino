8c7d9464cae457edfe7f709508beeaffa5845cb2
lihaolin-hit <2655254368@qq.com>
2024-01-13 21:47:58 +0800

    ***version2.0.1
    servo test

--- Servo.ino
+++ Servo.ino
@@ -1,24 +1,25 @@
-/*
 #include <Servo.h>
+#define servo_up_max 100
+#define servo_up_min 90
+#define servo_mid_max 100
+#define servo_mid_min 90 
+#define servo_down_max 100
+#define servo_down_min 90
 
-Servo myservo;  // create servo object to control a servo
+Servo Sservo_up;  // create servo object to control a servo
+Servo Sservo_mid;
+Servo Sservo_down;
 
-int potpin = A0;  // analog pin used to connect the potentiometer
-int val;    // variable to read the value from the analog pin
-
-void servo_setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+void Servo_Setup() {
+  Sservo_up.attach(9);  // attaches the servo on pin 9 to the servo object
+  /*Sservo_mid.attach(9);
+  Sservo_down.attach(9);*/
 }
 
-void servo_loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+void Servo_Loop() {
+  for (int pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15 ms for the servo to reach the position
-  }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15 ms for the servo to reach the position
+    Sservo_up.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(150);                       // waits 15 ms for the servo to reach the position
   }
 }
-*/
