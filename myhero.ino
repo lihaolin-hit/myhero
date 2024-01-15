f2eaf5fea75e992918959e5aa1de99c5ca7ba06c
lihaolin-hit <2655254368@qq.com>
2024-01-15 18:15:39 +0800

    最终版（大铲斗）

--- myhero.ino
+++ myhero.ino
@@ -1,8 +1,8 @@
 /*变量定义区*/
 int PS2_mode = 0, Vx = 0, Vy = 0, servo_down = 5, servo_mid = 0, servo_up = 150, Vw = 0;
 int motor1 = 0, motor2 = 0, motor3 = 0, motor4 = 0;
-const int servo_down_max = 60, servo_down_min = 5, servo_mid_min = 0, servo_mid_max = 53, servo_up_min = 95, servo_up_max = 150;
-
+const int servo_down_max = 60, servo_down_min = 5, servo_mid_min = 0, servo_mid_max = 53, servo_up_min = 122, servo_up_max = 160;
+//
 void watch() // 串口调试
 {
 
@@ -34,7 +34,7 @@ void setup()
 void loop()
 {
   PS2_Loop(&PS2_mode, &Vx, &Vy, &servo_down, &servo_mid, &servo_up, &Vw);
-  Motor_Loop(Vx, Vy, Vw, &motor1, &motor2, &motor3, &motor4);
+  Motor_Loop(Vx,Vy,Vw,&motor1, &motor2, &motor3, &motor4);
   watch();
   Servo_Loop(servo_down, servo_mid, servo_up);
 }
